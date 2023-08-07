c3ff929c9ab7b77b8cbbb50b82c6c82a18f8652a
Subhangam Dey <deysubhangam@gmail.com>
2023-08-07 19:44:18 +0530

    Added random dice functionality

---
+++ index.js
@@ -0,0 +1,47 @@
+const randomNumber1 = Math.ceil(Math.random()*6)
+
+switch(randomNumber1){
+    case 1:
+        document.querySelector(".img1").setAttribute("src","/images/dice1.png")
+        break;
+    case 2:
+        document.querySelector(".img1").setAttribute("src","/images/dice2.png")
+        break;
+    case 3:
+        document.querySelector(".img1").setAttribute("src","/images/dice3.png")
+        break;
+    case 4:
+        document.querySelector(".img1").setAttribute("src","/images/dice4.png")
+        break;
+    case 5:
+        document.querySelector(".img1").setAttribute("src","/images/dice5.png")
+        break;
+    case 6:
+        document.querySelector(".img1").setAttribute("src","/images/dice6.png")
+        break;
+}
+const randomNumber2 = Math.ceil(Math.random()*6)
+
+switch(randomNumber2){
+    case 1:
+        document.querySelector(".img2").setAttribute("src","/images/dice1.png")
+        break;
+    case 2:
+        document.querySelector(".img2").setAttribute("src","/images/dice2.png")
+        break;
+    case 3:
+        document.querySelector(".img2").setAttribute("src","/images/dice3.png")
+        break;
+    case 4:
+        document.querySelector(".img2").setAttribute("src","/images/dice4.png")
+        break;
+    case 5:
+        document.querySelector(".img2").setAttribute("src","/images/dice5.png")
+        break;
+    case 6:
+        document.querySelector(".img2").setAttribute("src","/images/dice6.png")
+        break;
+}
+
+
+
